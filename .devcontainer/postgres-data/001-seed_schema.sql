3c17c54546782045bd6fba131ede88903f9176b5
Eklavya Mirani <github1@eklavyamirani.com>
2025-04-13 23:10:34 +0000

    setup devcontainer to seed database.

---
+++ .devcontainer/postgres-data/001-seed_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS public.saved_links (
+    id SERIAL PRIMARY KEY,
+    link VARCHAR(1000) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    read_date TIMESTAMP NULL,
+    ip_address VARCHAR(45),
+    title VARCHAR(255)
+);
